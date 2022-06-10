a7a1c3d455efa2d955bfb90d66919448b724a476
Vitor <moutimg@gmail.com>
2022-06-10 18:53:11 -0300

    feat: cria pasta de rotas e linka elas na raiz do projeto

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 
 app.use(express.json());
 
+const routes = require('./routes/routes');
+app.use(routes);
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Rodando na porta ${PORT}`);
