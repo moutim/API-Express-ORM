3578683da01bf75006b29df65b91ddd5abfe9478
Vitor <moutimg@gmail.com>
2022-06-11 03:37:01 -0300

    fix: arruma bug no tratamento de erro

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,11 +1,12 @@
 const errorHandler = (error, req, res, next) => {
-  let erro = { status: 500, message: 'Erro interno.' }
+  let erro = { status: 500, message: 'Erro interno.'};
 
-  if (error.message && !error.errno) {
+  try {
     erro = JSON.parse(error.message);
-  };
-  
-  res.status(erro.status).json({ message: erro.message });
+    res.status(erro.status).json({ message: erro.message });
+  } catch(e) {
+    res.status(erro.status).json({ message: erro.message });
+  }
 
   next();
 };
