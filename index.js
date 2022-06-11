a5e0a50e7ecaa889486f962a8b8f53f05c424c12
Vitor <moutimg@gmail.com>
2022-06-11 02:27:02 -0300

    feat: chama middleware tratamento de erro

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+require('express-async-errors');
+const middlewares = require('./middlewares');
 
 const app = express();
 
@@ -6,6 +8,8 @@ app.use(express.json());
 
 const routes = require('./routes/routes');
 app.use(routes);
+
+app.use(middlewares.errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
